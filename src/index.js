deac25834969718f1bb925a27b56efed42a06a30
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-09 09:58:20 +0300

    get likes from api and store in an object

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './style.css';
 
 const FOOD_API_BASE_URL = 'https://www.themealdb.com/api/json/v1/1/';
 const ALL_FOOD_ENDPOINT = 'filter.php?a=Jamaican';
-
 const ALL_FOOD_API_URL = FOOD_API_BASE_URL + ALL_FOOD_ENDPOINT;
 
 const foodListWrapper = document.getElementById('home');
@@ -34,4 +33,21 @@ function getAllFoodDataAndRender() {
   });
 }
 
+const INV_API_BASE = 'https://us-central1-involvement-api.cloudfunctions.net/'
+  + 'capstoneApi/apps/';
+const INV_API_KEY = 'zX9lc5HNiZeTfJrwouGw';
+const LIKES_ENDPOINT = '/likes';
+
+const likes = {};
+
+function getAllLikes() {
+  const ALL_LIKES_API_URL = INV_API_BASE + INV_API_KEY + LIKES_ENDPOINT;
+
+  const likesObjects = getData(ALL_LIKES_API_URL);
+  likesObjects.forEach((likeObject) => {
+    likes[likeObject.item_id] = likeObject.likes;
+  });
+}
+
 getAllFoodDataAndRender();
+getAllLikes();
