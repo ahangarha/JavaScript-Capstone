65ea33a7306dc0be73bec63760800dc86ee6764f
daudi13 <davouma13@gmail.com>
2022-03-09 18:41:01 +0300

    Show comments on popUp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import FoodList from './modules/food-list.js';
 
 import './style.css';
 
+const foodList = new FoodList();
+const INV_API_BASE = 'https://us-central1-involvement-api.cloudfunctions.net/'
+  + 'capstoneApi/apps/';
+const INV_API_KEY = 'zX9lc5HNiZeTfJrwouGw';
+const LIKES_ENDPOINT = '/likes';
+const COMMENT_ENDPOINT = '/comments';
+
 const FOOD_API_BASE_URL = 'https://www.themealdb.com/api/json/v1/1/';
 const ALL_FOOD_ENDPOINT = 'filter.php?a=Jamaican';
 const ALL_FOOD_API_URL = FOOD_API_BASE_URL + ALL_FOOD_ENDPOINT;
@@ -10,45 +17,64 @@ const ALL_FOOD_API_URL = FOOD_API_BASE_URL + ALL_FOOD_ENDPOINT;
 const foodListWrapper = document.getElementById('home');
 const commentPopup = document.getElementById('comment-popup');
 
+function getComments(id) {
+  return new Promise((resolve) => {
+    const parameter = `?item_id=${id}`;
+    const COMMENT_API = INV_API_BASE + INV_API_KEY + COMMENT_ENDPOINT + parameter;
+    getData(COMMENT_API).then((commentsFromAPI) => {
+      console.log(commentsFromAPI);
+      foodList.addComments(id, commentsFromAPI);
+      resolve();
+    });
+  });
+}
+
 const displayPopUp = (id) => {
+  commentPopup.classList.add('show');
+  commentPopup.innerHTML = `      <div id="close-popup-button">&times;</div>
+    <div class="comment-popup-wrapper">
+      <img id="food-image" src="${foodList.foods[id].image}" alt="" />
+      <h2 id="food-title">${foodList.foods[id].title}</h2>
+      <p id="food-description">...</p>
+
+      <h3>Comments:</h3>
+      <ul id="comments">
+      </ul>
+
+      <h3>Add Comment:</h3>
+      <form id="comment-form">
+        <input type="text" name="name" placeholder="Your Name" />
+        <textarea
+          name="comment"
+          rows="5"
+          placeholder="Your Comment"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>`;
+  const closeBtn = document.getElementById('close-popup-button');
+  closeBtn.addEventListener('click', () => {
+    commentPopup.classList.remove('show');
+  });
+  // fetching more details
   const URL = `${FOOD_API_BASE_URL}lookup.php?i=${id}`;
   getData(URL).then((res) => {
     const foodItem = res.meals[0];
-    commentPopup.classList.add('show');
-    commentPopup.innerHTML = `      <div id="close-popup-button">&times;</div>
-      <div class="comment-popup-wrapper">
-        <img id="food-image" src="${foodItem.strMealThumb}" alt="" />
-        <h2 id="food-title">${foodItem.strMeal}</h2>
-        <p id="food-description">${foodItem.strInstructions}</p>
-
-        <h3>Comments:</h3>
-        <ul id="comments">
-          <li class="comment">
-            <h4 class="comment-author">#</h4>
-            <p class="comment-message">#</p>
-          </li>
-        </ul>
-
-        <h3>Add Comment:</h3>
-        <form id="comment-form">
-          <input type="text" name="name" placeholder="Your Name" />
-          <textarea
-            name="comment"
-            rows="5"
-            placeholder="Your Comment"
-          ></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </div>`;
-
-    const closeBtn = document.getElementById('close-popup-button');
-    closeBtn.addEventListener('click', () => {
-      commentPopup.classList.remove('show');
+    const foodDescElement = document.getElementById('food-description');
+    foodDescElement.innerHTML = foodItem.strInstructions;
+  });
+  getComments(id).then(() => {
+    const { comments } = foodList.foods[id];
+    console.log({ comments });
+    const commentWrapper = document.getElementById('comments');
+    comments.forEach((comment) => {
+      commentWrapper.innerHTML += `<li class="comment">
+      <h4 class="comment-author">${comment.username}</h4>
+      <p class="comment-message">${comment.comment}</p>
+      </li> `;
     });
   });
 };
-
-const foodList = new FoodList();
 
 function showAllFood() {
   // clear loading text
@@ -70,6 +96,12 @@ function showAllFood() {
     </div>
     `;
   });
+  const commentsButtons = foodListWrapper.querySelectorAll('.comments-button');
+  commentsButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      displayPopUp(btn.parentElement.id);
+    });
+  });
 }
 
 function getAllFoodData() {
@@ -85,19 +117,8 @@ function getAllFoodData() {
       });
       resolve();
     });
-    const commentsButtons = foodListWrapper.querySelectorAll('.comments-button');
-    commentsButtons.forEach((btn) => {
-      btn.addEventListener('click', () => {
-        displayPopUp(btn.parentElement.id);
-      });
-    });
   });
 }
-
-const INV_API_BASE = 'https://us-central1-involvement-api.cloudfunctions.net/'
-  + 'capstoneApi/apps/';
-const INV_API_KEY = 'zX9lc5HNiZeTfJrwouGw';
-const LIKES_ENDPOINT = '/likes';
 
 function getAllLikes() {
   return new Promise((resolve) => {
