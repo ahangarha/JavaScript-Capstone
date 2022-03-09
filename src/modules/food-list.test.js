6f12e9b9351243546bd82505ebc371bbb38bcf85
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-09 21:06:09 +0300

    improve test for adding multiple food

--- src/modules/food-list.test.js
+++ src/modules/food-list.test.js
@@ -1,13 +1,24 @@
 import FoodList from './food-list.js';
 
-test('add new food', () => {
+describe('Get likes', () => {
   const foodList = new FoodList();
-  const id = '1';
-  const title = 'the title';
-  const imageUrl = 'image url';
 
-  foodList.addFood(id, title, imageUrl);
+  test('add new food', () => {
+    const id = '1';
+    const title = 'the title';
+    const imageUrl = 'image url';
 
-  expect(foodList.foods[id].title).toBe(title);
-  expect(foodList.foods[id].image).toBe(imageUrl);
+    foodList.addFood(id, title, imageUrl);
+
+    expect(foodList.foods[id].title).toBe(title);
+    expect(foodList.foods[id].image).toBe(imageUrl);
+  });
+
+  test('add extra food', () => {
+    foodList.addFood('2', 'second food', 'second image');
+    const numberOfFood = Object.keys(foodList.foods).length;
+
+    expect(numberOfFood).toBe(2);
+    expect(foodList.foods['2'].title).toBe('second food');
+  });
 });
