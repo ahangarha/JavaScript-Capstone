4128798ea6565cc8bacbd5424677d1e85888fbb0
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-13 18:20:57 +0300

    improve popup design for desktop

--- src/modules/core-functions.js
+++ src/modules/core-functions.js
@@ -52,26 +52,28 @@ export const displayPopUp = (id) => {
   commentPopup.classList.add('show');
   commentPopup.innerHTML = `      <div id="close-popup-button">&times;</div>
     <div class="comment-popup-wrapper">
-      <img id="food-image" src="${foodList.foods[id].image}" alt="" />
-      <h2 id="food-title">${foodList.foods[id].title}</h2>
-      <p id="food-description">...</p>
+      <div class="info-section">
+        <img id="food-image" src="${foodList.foods[id].image}" alt="" />
+        <h2 id="food-title">${foodList.foods[id].title}</h2>
+        <p id="food-description">...</p>
+      </div>
+      <div class="comments-section">
+        <h3>Add Comment:</h3>
+        <form id="comment-form">
+          <input type="text" name="name" placeholder="Your Name" required />
+          <textarea
+            name="comment"
+            rows="5"
+            placeholder="Your Comment"
+            required
+          ></textarea>
+          <button id="button" type="submit">Submit</button>
+        </form>
 
-      <h3 id="comments-header">Comments:</h3>
-      <ul id="comments">
-      </ul>
-
-      <h3>Add Comment:</h3>
-      <form id="comment-form">
-        <input type="text" name="name" id="nameField" placeholder="Your Name" required />
-        <textarea
-          name="comment"
-          rows="5"
-          placeholder="Your Comment"
-          required
-          id="messageField"
-        ></textarea>
-        <button id="button" type="submit">Submit</button>
-      </form>
+        <h3 id="comments-header">Comments:</h3>
+        <ul id="comments">
+        </ul>
+      </div>
     </div>`;
 
   const commentForm = document.getElementById('comment-form');
